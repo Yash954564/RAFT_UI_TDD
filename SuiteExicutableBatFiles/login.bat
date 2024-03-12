87140d633605b23114b0504fd8f3355c776dafa4
Yash Gaikwad <91732468+Yash954564@users.noreply.github.com>
2024-03-12 11:02:42 +0530

    Update login.bat

--- SuiteExicutableBatFiles/login.bat
+++ SuiteExicutableBatFiles/login.bat
@@ -7,3 +7,5 @@ cd..
 
 pytest ./TestScripts/test_login.py --alluredir=ExecutionReports/Login/"%date:/=-%"
 
+allure generate ExecutionReports/Login/"%date:/=-%" -o ExecutionReports/Login/"%date:/=-%"/"%time::=-%"
+
